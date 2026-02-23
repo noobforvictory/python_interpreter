b6170aa36ebe62912e1a8e0c919bf4abb2f9e600
Denis <denisvictorjosphine1996@gmail.com>
2026-02-23 10:53:01 +0530

    breake down how the intepretor will work in readme, use it as a plan for the next stage

---
+++ README.md
@@ -0,0 +1,10 @@
+Components of interpretor->
+1. take  human readabke commands in text (make your own language that is consistent)
+
+2. convert it into bytecode
+    - You need to parse the instructions
+    -build sequence of exicutable bytecode instructions
+    - once the first two are done, you need to pass on this bytecode to the the interpretor for exicution.
+    
+3. convert it into human readable bytecode instructions.
+have interpretor functions that can implement all the commands that can be present in the byte code or the features we want to support in our interpretor.
--- main.py
+++ main.py
@@ -1,17 +1,7 @@
 from interpreter import Interpreter
 
 
-# what_to_execute = {
-#         "instructions": [("LOAD_VALUE", 0),
-#                          ("STORE_NAME", 0),
-#                          ("LOAD_VALUE", 1),
-#                          ("STORE_NAME", 1),
-#                          ("LOAD_NAME", 0),
-#                          ("LOAD_NAME", 1),
-#                          ("ADD_TWO_VALUES", None),
-#                          ("PRINT_ANSWER", None)],
-#         "numbers": [1, 2],
-#         "names":   ["a", "b"] }
+
 
 what_to_execute = {
         "instructions": [("LOAD_VALUE", 0),
