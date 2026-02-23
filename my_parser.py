1400cfbfe3b6413adaf2db06fa155369cf716ed0
Denis <denisvictorjosphine1996@gmail.com>
2026-02-23 12:51:15 +0530

    Convert the string instruction into a array whoes elements are the integers which will be operated upon and operations which will be used on these integers

--- my_parser.py
+++ my_parser.py
@@ -1,3 +1,30 @@
 
+import array
+
+
 def parse(program: str):
-    return None
+    start = 0
+    end = 0
+    arr = []
+    for i in range(1,len(program)):
+        if not program[i].isnumeric():
+            end = i-1
+            if start == end:
+                arr.append(program[start])
+            else:
+                arr.append(program[start: end+1])
+            start = i+1
+            arr.append(program[i])
+    arr.append(program[-1])
+    print(arr)
+
+   
+
+
+
+def main():
+    parse("8+10/2*2-1")
+
+
+if __name__ == "__main__":
+    main()
